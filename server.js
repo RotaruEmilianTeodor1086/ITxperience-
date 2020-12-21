abe093c1d12fd2b24379cde39d000770859cc34f
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2020-12-21 10:29:38 +0200

    Dependente

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize') 
+const { Sequelize } = require('sequelize')
 
-
+const sequelize = new Sequelize()
 
 const app = express() 
 app.use(bodyParser.json())
