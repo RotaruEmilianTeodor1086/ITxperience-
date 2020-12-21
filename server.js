363b8889b9f2b715ce21ee326c2e6a6ca1426449
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2020-12-21 11:16:23 +0200

    Clau_the_best

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { Sequelize } = require('sequelize')
+const Sequelize  = require('sequelize')
 
 const sequelize = new Sequelize('my_database','app','clau_the_best',{
 
     dialect:'mysql'
 
 } )
+const Note= sequelize('note',{
+    idNote:{
+        type: Sequelize.INTEGER
+    },
+    title:{
+        type: Sequelize.STRING  
+    },    
+    text:{
+        type: Sequelize.STRING
+
+    },
+    image:{
+        type: Sequelize.BLOB
+    },
+    date:{
+        type: Sequelize.STRING
+    },
+    email:{
+        type: Sequelize.STRING 
+
+    }
+})
+const Subject = sequelize('subject',{
+    idSubject:{
+        type: Sequelize.INTEGER
+    },
+    name:{
+        type: Sequelize.STRING
+    },
+    subjectType:{
+        type: Sequelize.ENUM , 
+        allowNull: false,
+        values: ['CURS','SEMINAR','ACTIVITATE_INDIVIDUALA']
+    },
+    teacher:{
+        type: Sequelize.STRING,
+        defaultValue: "Toma"
+    }
+})
+Subject.hasMany(Note)
+
 
 const app = express() 
 app.use(bodyParser.json())
