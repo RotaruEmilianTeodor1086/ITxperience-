17220f59baf1887474d4fc44bdaf1072ffec6750
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2020-12-21 10:35:35 +0200

    sada

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize()
+const sequelize = new Sequelize('my_database','app','clau_the_best',{
+
+    dialect:'mysql'
+
+} )
 
 const app = express() 
 app.use(bodyParser.json())
