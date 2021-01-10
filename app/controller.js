3cd75ddf4fff825b8d29e859784c21f640d2c1d6
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-01-11 00:15:47 +0200

    backend

---
+++ app/controller.js
@@ -0,0 +1,60 @@
+const{createNode,fetchAllNotes,updateNote,deleteNote}= require('./model') ; 
+const {getTime}= require('./helper') ;
+exports.createNewNote= async(req,res)=>{
+try{
+    let currentTime = getTime() ; 
+    let newNote= {
+        title: 'Untitled',
+        description:'',
+        creationDate: currentTime,
+        updatedDate: currentTime
+
+    }
+    let id = await this.createNewNote(new Note) ;
+    newNote['_id'] = id ; 
+    res.status(200).send(newNote) ;  
+}
+catch(err){
+    res.status(400).send(err.message) ; 
+}
+}
+exports.getAllNotes = async(req,res) =>{
+    try{
+        let query = {
+            archive:0
+        } ; 
+        if(req.params.type=='trash'){
+            query.archive=1 ; 
+        }
+        let data = await fetchAllNotes(query) ; 
+        res.status(200).send(data) ; 
+
+    }
+    catch(err){
+        res.status(400).send(err.message) ; 
+    }
+}
+exports.updateNoteById = async (req,res) =>{
+    try{
+        let currentTime= getTime() ; 
+        let query={
+            ...req.body,
+            updatedDate: currentTime
+        }
+        await this.updateNoteById(req.params.id,query) ; 
+        req.status(200).send(query)
+
+    }
+    catch(err){
+        res.status(400).send(err.message) ; 
+    }
+}
+exports.deleteNote = async(req,res) =>{
+    try{
+        await this.deleteNote(req.params.id) ; 
+        res.status(200).send(id) ; 
+    }
+    catch(err){
+        res.status(400).send(err.message) ; 
+    }
+}
