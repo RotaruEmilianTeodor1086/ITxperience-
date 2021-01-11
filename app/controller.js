39de64d613b8cb5f72d8247943db93f437ece045
iuliasilinescu19 <62059180+iuliasilinescu19@users.noreply.github.com>
2021-01-11 23:12:28 +0200

    PIERSICUTA

--- app/controller.js
+++ app/controller.js
@@ -1,63 +1,63 @@
-const{createNote,fetchAllNotes,updateNote,deleteNote}= require('./model'); 
-const {getTime}= require('./helper') ;
-exports.createNewNote= async(req,res)=>{
-try{
+const { createNote, fetchAllNotes, updateNote, deleteNote } = require('./model');
+const { getTime } = require('./helper');
 
-    let currentTime = getTime() ;  
-    let newNote= {
-        title: 'Untitled',
-        description:'',
-        creationDate: currentTime,
-        updatedDate: currentTime,
-        archive:0
-    };
-    let id = await createNote(newNote) ;
-    newNote['_id'] = id ; 
-    res.status(200).send(newNote) ; 
-
-}
-catch(err){
-
-    res.status(400).send(err.message) ; 
-}
-}
-
-exports.getAllNotes = async(req,res) =>{
-    try{
-        let query = {
-            archive:0
-        } ; 
-        if(req.params.type=='trash'){
-            query.archive=1 ; 
+exports.createNewNote = async (req, res) => {
+    try {
+        let currentTime = getTime();
+        let newNote = {
+            title: 'Untitled',
+            desc: '',
+            creationDate: currentTime,
+            updatedDate: currentTime,
+            archive: 0
         }
-        let data = await fetchAllNotes(query) ; 
-        res.status(200).send(data) ; 
+        let id = await createNote(newNote);
+        newNote['_id'] = id;
+        res.status(200).send(newNote);
     }
-    catch(err){
-        res.status(400).send(err.message) ; 
+    catch (ex) {
+        res.status(400).send(ex.message)
     }
 }
-exports.updateNoteById = async (req,res) =>{
-    try{
-        let currentTime= getTime() ; 
-        let query={
+
+exports.getAllNotes = async (req, res) => {
+    try {
+        let query = {
+            archive: 0
+        };
+        if (req.params.type === 'trash') {
+            query.archive = 1;
+        }
+        let data = await fetchAllNotes(query);
+        res.status(200).send(data);
+    }
+    catch (ex) {
+        res.status(400).send(ex.message)
+    }
+}
+
+exports.updateNoteById = async (req, res) => {
+    try {
+        let currentTime = getTime();
+        let query = {
             ...req.body,
             updatedDate: currentTime
         }
-        await this.updateNoteById(req.params.id,query) ; 
-        req.status(200).send(query)
-
+        await updateNote(req.params.id, query);
+        res.status(200).send(query);
     }
-    catch(err){
-        res.status(400).send(err.message) ; 
+    catch (ex) {
+        res.status(400).send(ex.message)
     }
 }
-exports.deleteNote = async(req,res) =>{
-    try{
-        await this.deleteNote(req.params.id) ; 
-        res.status(200).send(req.params.id) ; 
+
+exports.deleteNote = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await deleteNote(id);
+        res.status(200).send(id);
     }
-    catch(err){
-        res.status(400).send(err.message) ; 
+    catch (ex) {
+        res.status(400).send(ex.message)
     }
 }
