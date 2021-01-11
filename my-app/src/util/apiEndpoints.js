3fbae98a8f21747faf7807064a4471a48558ba9e
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-01-11 16:59:27 +0200

    xzczxczx

--- my-app/src/util/apiEndpoints.js
+++ my-app/src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL ='http://localhost:2000' ; 
+export const BASE_URL ='http://localhost:2001' ; 
 export const CREATE_NOTE = '/api/note' ; 
 export const UPDATE_NOTE = '/api/note/' ; 
 export const DELETE_NOTE = '/api/note/' ; 
--- server2.js
+++ server2.js
@@ -1,12 +1,12 @@
 require('dotenv').config() ; 
 const express = require('express')
-const app = express()
-let bodyParser = require('body-parser')  
+const app = express()  
+let bodyParser = require('body-parser') ;  
 let cors = require('cors')
-const port = 2000 
+const port = 2001 ;
 app.use(cors())  
-app.use(bodyParser.json())  
-app.use(bodyParser.urlencoded({extended: true}))  
+app.use(bodyParser.json())  ;
+app.use(bodyParser.urlencoded({extended: true}))  ;
 require(`${__dirname}/app/routes`)(app)  
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
