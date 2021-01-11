994592aa6b9b0ad01e2849468b2278645bd51bff
iuliasilinescu19 <62059180+iuliasilinescu19@users.noreply.github.com>
2021-01-11 13:14:01 +0200

    ulalalala

--- my-app/src/components/Note/Note.js
+++ my-app/src/components/Note/Note.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Note.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArchive} from '@fortawesome/free-solid-svg-icons';
+
 
 function Note() {
   return (
-    <div className="Note">
+    <div className="EditableNote">
+        <div className="EditableNote_Top">
+            <div className="EditableNote_Top_Date">Ultima editare la 11 ianuarie,2020</div>
+            <div className="EditableNote_Top_Buttons">
+                 <div className="action_buttons">
+                     <FontAwesomeIcon id="idArchive" icon={faArchive}></FontAwesomeIcon>
+                 </div>
+            </div>
+        </div>
+        <div className="EditableNote_Middle">
+             <div className="EditableNote_Middle_Top">
+                  <input placeholder="Titlu notita"></input>
+             </div>
+             <div className="EditableNote_Middle_Content">
+                  <textarea id="idTextArea" placeholder="Scrie continutul notitei aici..."></textarea>
+             </div>
+        </div>
     </div>
   );
 }
